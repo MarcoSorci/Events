932b01eef90ee61d6e187cc833f9f074f458cb4b
MarcoSorci <97511096+MarcoSorci@users.noreply.github.com>
2022-02-24 13:22:29 +0100

    evvs

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 
+// setTimeout(() => {
+//     console.log("peee");
+// }, 10000);  //in milliseconds, + 000 to seconds ofc
+
+
+// let intervalCounter = 0;
+// const interval = setInterval(() => {
+//     console.log(intervalCounter);
+//     if (intervalCounter === 10) {
+//         clearInterval(interval)
+//     }
+//     intervalCounter++
+// }, 1000);
+
+
+
+
 const button3 = document.getElementById('button3')
 
 button3.onclick = () => buttonClick("button3")
@@ -9,13 +26,14 @@ const button4 = document.getElementById('button4')
 
 button4.addEventListener('mousedown', countClicks) //with addEventListener we can do more functions on the same event
 
-document.addEventListener('keydown', changeBackground)
+document.addEventListener('keydown', realTimeSearch)
 
 const parPippo = document.getElementById('par-pippo')
 parPippo.addEventListener('click', countClicks)
 
 const link = document.getElementById('link')
 link.addEventListener('click', displayPar)
+
 
 function buttonClick(element) {
     console.log(element);
@@ -48,3 +66,44 @@ function changeBackground(event) {
         document.body.style.backgroundColor = '#' + num + num + num + num + num + num + num + num
     }
 }
+
+
+function onMouseEnter() {
+    console.log("I'm in");
+}
+
+function onMouseLeave() {
+    console.log("I'm out");
+}
+
+const div = document.getElementById('mouse-div-2')
+
+// div.addEventListener('mouseenter', onMouseEnter)
+// div.addEventListener('mouseleave', onMouseLeave)
+
+div.addEventListener('mousemove', onMouseOver)
+
+function onMouseOver() {
+    console.log("I'm over it");
+}
+
+
+let selectedKey=""
+function onKeyDown(event) {
+    if (event.key !== selectedKey) {
+        console.log("pressed " + event.key + " key");
+        selectedKey = event.key
+    }
+}
+
+let searchTimeout
+
+function realTimeSearch(event) {
+    if (searchTimeout) {
+        clearTimeout(searchTimeout)
+    }
+   searchTimeout = setTimeout(() => {
+       console.log("I'm searchin bro");
+   }, 1000); 
+}
+
